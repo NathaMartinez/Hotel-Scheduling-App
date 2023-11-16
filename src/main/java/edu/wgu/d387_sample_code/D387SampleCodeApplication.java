f9e3fef96df101d1950a6d8696d1192ede904d9a
Nathan Martinez <nmar502@wgu.edu>
2023-11-15 18:53:57 -0700

    Display the welcome messages in both English and French using different threads
    Task C1.b: Call SearchBundle class in main method and print to console

--- src/main/java/edu/wgu/d387_sample_code/D387SampleCodeApplication.java
+++ src/main/java/edu/wgu/d387_sample_code/D387SampleCodeApplication.java
@@ -1,15 +1,59 @@
 package edu.wgu.d387_sample_code;
 
+import edu.wgu.d387_sample_code.language.SearchBundle;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.io.ClassPathResource;
+import java.io.IOException;
+import java.io.InputStream;
 
 @SpringBootApplication
 public class D387SampleCodeApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(D387SampleCodeApplication.class, args);
-	}
 
+		SearchBundle searchBundle = new SearchBundle();
+
+		try {
+			InputStream inputStream = new ClassPathResource("welcome_CA.properties").getInputStream();
+			String[] welcomeMessages = searchBundle.getWelcomeMessage(inputStream);
+				for(String message : welcomeMessages) {
+					System.out.println(message);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		try {
+			InputStream inputStream = new ClassPathResource("welcome_en_US.properties").getInputStream();
+			String[] welcomeMessages = searchBundle.getWelcomeMessage(inputStream);
+				for(String message : welcomeMessages) {
+					System.out.println(message);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+			/*
+		Properties properties=new Properties();
+		messageExecutor.execute(()-> {
+			try {
+				InputStream stream = new ClassPathResource("welcome_CA.properties").getInputStream();
+				properties.load(stream);
+				System.out.println(properties.getProperty("welcomeMessage"));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		});
+		messageExecutor.execute(()-> {
+			try {
+				InputStream stream = new ClassPathResource("welcome_en_US.properties").getInputStream();
+				properties.load(stream);
+				System.out.println(properties.getProperty("welcomeMessage"));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		});
+	}
+			 */
+	}
 }
