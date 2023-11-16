956b0eea3c98d1d05e68dddcf803f73d135d0620
Nathan Martinez <nmar502@wgu.edu>
2023-11-15 19:55:46 -0700

    Build REST controller to create endpoints for each message
    Task C1.b: Use endpoints to return welcome messages that can later be called by front-end components

---
+++ src/main/java/edu/wgu/d387_sample_code/rest/MessageController.java
@@ -0,0 +1,32 @@
+package edu.wgu.d387_sample_code.rest;
+
+import edu.wgu.d387_sample_code.language.SearchBundle;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+
+@RestController
+    @RequestMapping("/api/messages")
+    public class MessageController {
+
+        @GetMapping("/welcome-fr")
+        public String[] getWelcomeMessageFR() throws IOException {
+            SearchBundle searchBundle = new SearchBundle();
+            InputStream inputStream = new ClassPathResource("welcome_CA.properties").getInputStream();
+            return searchBundle.getWelcomeMessage(inputStream);
+
+        }
+    @GetMapping("/welcome-en")
+    public String[] getWelcomeMessageEN() throws IOException {
+        SearchBundle searchBundle = new SearchBundle();
+        InputStream inputStream = new ClassPathResource("welcome_en_US.properties").getInputStream();
+        return searchBundle.getWelcomeMessage(inputStream);
+
+    }
+
+}
