b795c85ac548520a3ab5055901dd3e797c6a1922
Nathan Martinez <nmar502@wgu.edu>
2023-11-21 07:08:13 -0700

    Implement CrossOrigin support

--- src/main/java/edu/wgu/d387_sample_code/rest/MessageController.java
+++ src/main/java/edu/wgu/d387_sample_code/rest/MessageController.java
@@ -2,6 +2,7 @@ package edu.wgu.d387_sample_code.rest;
 
 import edu.wgu.d387_sample_code.language.SearchBundle;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,18 +10,18 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.IOException;
 import java.io.InputStream;
 
-
+@CrossOrigin
 @RestController
-    @RequestMapping("/api/messages")
-    public class MessageController {
+@RequestMapping("/api/messages")
+public class MessageController {
 
-        @GetMapping("/welcome-fr")
-        public String[] getWelcomeMessageFR() throws IOException {
-            SearchBundle searchBundle = new SearchBundle();
-            InputStream inputStream = new ClassPathResource("welcome_CA.properties").getInputStream();
-            return searchBundle.getWelcomeMessage(inputStream);
+    @GetMapping("/welcome-fr")
+    public String[] getWelcomeMessageFR() throws IOException {
+        SearchBundle searchBundle = new SearchBundle();
+        InputStream inputStream = new ClassPathResource("welcome_CA.properties").getInputStream();
+        return searchBundle.getWelcomeMessage(inputStream);
 
-        }
+    }
     @GetMapping("/welcome-en")
     public String[] getWelcomeMessageEN() throws IOException {
         SearchBundle searchBundle = new SearchBundle();
